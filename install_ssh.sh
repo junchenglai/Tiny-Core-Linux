7e57ad6e0176ad279f806e961811159e4f0133ed
赖俊成 <jun@cheng.studio>
2018-12-05 01:35:14 +0800

    1526

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,7 +1,5 @@
 # 配置 DNS 信息
 sudo echo 'nameserver 114.114.114.114' >> /etc/resolv.conf
-sudo echo 'nameserver 1.1.1.1' >> /etc/resolv.conf
-sudo echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
 
 # 安装 OpenSSH 包
 tce-load -wi openssh
@@ -22,6 +20,10 @@ sudo echo '/etc/shadow' >> /opt/.filetool.lst
 
 # 编辑启动文件
 sudo chmod -R 777 /opt/bootlocal.sh
+sudo echo 'sudo echo nameserver 114.114.114.114 >> /etc/resolv.conf' >> /opt/bootlocal.sh
+sudo echo 'sudo echo nameserver 1.1.1.1 >> /etc/resolv.conf' >> /opt/bootlocal.sh
+sudo echo 'sudo echo nameserver 8.8.8.8 >> /etc/resolv.conf' >> /opt/bootlocal.sh
+
 sudo echo 'echo tc:password | chpasswd' >> /opt/bootlocal.sh
 sudo echo 'sudo cp /usr/local/etc/ssh/sshd_config.orig /usr/local/etc/ssh/sshd_config' >> /opt/bootlocal.sh
 sudo echo 'sudo /usr/local/etc/init.d/openssh start' >> /opt/bootlocal.sh
